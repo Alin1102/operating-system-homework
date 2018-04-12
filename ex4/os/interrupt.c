9c7e6c9332dac3a1ab4cbbd97aa504be989b3285
谢江钊 <10751583@qq.com>
2018-04-12 22:06:39 +0800

    v0.71
    Let's try

--- ex4/os/interrupt.c
+++ ex4/os/interrupt.c
@@ -3,11 +3,23 @@ __asm__(".code16gcc\n");
 #include "string.h"
 char* Typing="Typing...";
 char* Type="Type Free";
-char* mytest="Test";
+char* frame_char="=";
 char* Int34h_info="34H know that!";
 char* Int35h_info="35H know that!";
 char* Int36h_info="36H know that!";
 char* Int37h_info="37H know that!";
+
+void Print_Frame(int x1,int y1,int x2,int y2){
+    int i=x1;
+    for(int i=y1;i<=y2;i++){
+        WriteStr(frame_char,x1,i,len(frame_char),15);
+        WriteStr(frame_char,x2,i,len(frame_char),15);
+    }
+    for(int i=x1;i<=x2;i++){
+        WriteStr(frame_char,i,y1,len(frame_char),15);
+        WriteStr(frame_char,i,y2,len(frame_char),15);
+    }
+}
 void Print_Typing(){
     WriteStr(Typing,24,0,9,15);
 }
@@ -15,6 +27,7 @@ void Print_Type(){
     WriteStr(Type,24,0,9,15);
 }
 void Print_34H(){
+    Print_Frame(0,0,12,40);
     WriteStr(Int34h_info,10,20,len(Int34h_info),15);
 }
 void Print_35H(){
