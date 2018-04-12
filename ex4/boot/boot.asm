c277447ad7465581567d82422bddc4c6eaad9473
谢江钊 <10751583@qq.com>
2018-04-12 14:32:43 +0800

    V0.71
    系统扩容准备

--- ex4/boot/boot.asm
+++ ex4/boot/boot.asm
@@ -20,7 +20,7 @@ loados:
     mov ax,cs                ;段地址 ; 存放数据的内存基地址
     mov es,ax                ;设置段地址（不能直接mov es,段地址）   
     mov ah,2                 ;功能号
-    mov al,20                 ;扇区数
+    mov al,25                 ;扇区数
     mov dl,0                 ;驱动器号 ; 软盘为0，硬盘和U盘为80H
     mov dh,0                 ;磁头号 ; 起始编号为0
     mov ch,0                 ;柱面号 ; 起始编号为0
